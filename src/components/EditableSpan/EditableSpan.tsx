f67c9de2ce5211aa241fd9506bfcfa745d38742e
Andrey <challenger474@gmail.com>
2024-02-21 20:55:55 +0300

    use reducer

--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -38,3 +38,36 @@ export const EditableSpan = (props: EditableSpanProps) => {
     <span onDoubleClick={editFoo}>{props.oldTitle}</span>
   );
 };
+
+// import React, { ChangeEvent, useState } from 'react';
+
+// type EditableSpanProps = {
+//   oldTitle: string;
+//   callback: (newTitle: string) => void;
+// };
+
+// export const EditableSpan = (props: EditableSpanProps) => {
+//   const [edit, setEdit] = useState(false);
+//   const [newTitle, setNewTitle] = useState(props.oldTitle);
+
+//   const editFoo = () => {
+//     setEdit(!edit);
+//     if (edit) {
+//       addTask();
+//     }
+//   };
+
+//   const addTask = () => {
+//     props.callback(newTitle);
+//   };
+
+//   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+//     setNewTitle(e.currentTarget.value);
+//   };
+
+//   return edit ? (
+//     <input value={newTitle} onChange={onChangeHandler} onBlur={editFoo} autoFocus />
+//   ) : (
+//     <span onDoubleClick={editFoo}>{props.oldTitle}</span>
+//   );
+// };
